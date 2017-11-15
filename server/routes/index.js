cf4fccfc9c44143da55ad6385baa11216d84e300
warriorsword1989 <linglong@navinfo.com>
2017-11-15 14:27:26 +0800

    问题的创建

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,5 @@ var express = require('express');
 var router = express.Router();
 var token = require('../utils/checkToken');
 // 对api的所有接口进行token验证，除了login/register接口
-// router.use(/^((?!((register)|login)).)*$/, token);
+router.use(/^((?!((register)|login)).)*$/, token);
 module.exports = router;
