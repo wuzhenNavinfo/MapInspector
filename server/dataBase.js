42b2cb01b2f269747e7d7402f96f9b5ee6285b31
warriorsword1989 <linglong@navinfo.com>
2017-11-21 11:14:41 +0800

    修改时区

--- server/dataBase.js
+++ server/dataBase.js
@@ -2,16 +2,19 @@ const config = require('./config');
 const Sequelize = require('sequelize');
 
 module.exports = new Sequelize(
-    config.DB_NAME,
-    config.DB_USER,
-    config.DB_PWD,
-    {
-        host: config.DB_HOST,
-        dialect: 'mysql',
-        pool: {
-            max: 5,
-            min: 0,
-            idle: 10000
-        }
-    }
+  config.DB_NAME,
+  config.DB_USER,
+  config.DB_PWD,
+  {
+    host: config.DB_HOST,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    timezone: '+8:00',
+    timestamps: true,
+    underscoredAll: false,
+  }
 );
