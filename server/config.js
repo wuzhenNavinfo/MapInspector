a6e6aa9001b1d7894c4311da20d89cf808991829
wuzhen@navinfo.com <wuzhen@navinfo.com>
2017-12-07 09:41:55 +0800

    修改配置为开发环境

--- server/config.js
+++ server/config.js
@@ -2,10 +2,16 @@
 *   系统配置文件
 * */
 var config = {};
-// 数据库相关配置;
-config.DB_HOST = '192.168.15.220';  // 192.168.4.189
+// 正式环境的数据库相关配置;
+// config.DB_HOST = '192.168.15.220';
+// config.DB_USER = 'root';
+// config.DB_PWD = '';
+// config.DB_NAME = 'map_inspector';
+
+// 开发环境的数据库相关配置;
+config.DB_HOST = '192.168.4.189';
 config.DB_USER = 'root';
-config.DB_PWD = '';
+config.DB_PWD = '111111';
 config.DB_NAME = 'map_inspector';
 
 // 超级管理员邮箱设置;
